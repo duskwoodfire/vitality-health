1bb425d8e5989ab291c4b9123338c3cece7e7b96
Tennobis <tanveer2004hossain@gmail.com>
2025-02-02 17:54:45 +0530

    Slight Colour Changes

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -62,7 +62,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                   <div className="-mr-2">
                     <button
                       type="button"
-                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-[#334d2b] hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-zinc-500"
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-[#343a32] hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-zinc-500"
                       onClick={() => setIsMenuOpen(false)}
                     >
                       <span className="sr-only">Close menu</span>
